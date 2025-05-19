56c8822f5e2864e34a294a0101e1b816a6c85aff
Ivan Laptev <145211896+lystarblone@users.noreply.github.com>
2025-05-19 20:27:27 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (53.50%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        nums = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        output = ['']
+
+        for i in digits:
+            current_comb = nums[i]
+            new_comb = []
+            for j in output:
+                for k in current_comb:
+                    new_comb.append(j+k)
+            output = new_comb
+        return output
